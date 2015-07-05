3af01f4867093c96e3476abfc61f24a7ae2504d8
Wilson Horstmeyer Bogado <wilson@engeweb.com.br>
2015-07-05 11:41:11 -0300

    Inicial.

---
+++ src/main/java/Projeto1.java
@@ -0,0 +1,13 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * 
+ * @author 
+ */
+public class Projeto1 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ src/main/java/utfpr/ct/dainf/if62c/projeto/Ponto.java
@@ -0,0 +1,20 @@
+package utfpr.ct.dainf.if62c.projeto;
+
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ *
+ * @author
+ */
+public class Ponto {
+    private double x, y, z;
+
+    /**
+     * Retorna o nome não qualificado da classe.
+     * @return O nome não qualificado da classe.
+     */
+    public String getNome() {
+        return getClass().getSimpleName();
+    }
+
+}
